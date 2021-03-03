3043d42a24faee6a5406fbee5924239c2afeabb6
unknown <jmires00@gmail.com>
2021-03-02 17:04:19 -0700

    labtuesday

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
-
-class App extends Component {
+import './index.css';
+// import TopicBrowser from './components/TopicBrowser/TopicBrowser'
+import EvenAndOdd from './components/Topics/EvenAndOdd'
+import FilterObject from './components/Topics/FilterObject'
+import FilterString from './components/Topics/FilterString'
+import Palindrome from './components/Topics/Palindrome'
+import Sum from './components/Topics/Sum'
+export default class TopicBrowser extends Component {
   render() {
-    
+    return (
+      <div>
+        <EvenAndOdd />
+        <FilterObject/>
+        <FilterString/>
+        <Palindrome/>
+        <Sum/>
+      </div>
+    )
   }
 }
 
-export default App;
+
